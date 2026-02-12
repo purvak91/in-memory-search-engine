3982ccf83d31114190c818135830b3cb9728335c
Purva Khandelwal <purvakhandelwal91@gmail.com>
2026-02-12 17:20:07 +0530

    feat: initial project setup with Spring Boot

---
+++ src/main/java/com/purva/searchengine/InMemorySearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.purva.searchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemorySearchEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InMemorySearchEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/purva/searchengine/InMemorySearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purva.searchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMemorySearchEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
